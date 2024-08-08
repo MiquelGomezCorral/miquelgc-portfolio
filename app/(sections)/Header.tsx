afb5f7e6f0b285e2a3d03644868998edd8dc342d
MaikTheTurboPotato <miquelgc2003@gmail.com>
2024-08-08 23:01:12 +0200

    Header half-half-responsive

--- app/(sections)/Header.tsx
+++ app/(sections)/Header.tsx
@@ -12,16 +12,16 @@ export default function Header() {
   const [showMenu, setshowMenu] = useState(false)
   return (
     <>
-    <div
-      className="fixed z-50 top-4 right-6 rounded-full p-2 w-9 border hover:bg-miquel-black-300"
-      onClick={()=>setshowMenu(!showMenu)}
-    >
-    <IconText
-      src={!showMenu ? "menu-close" : "menu-open"} title={!showMenu ? CloseMenu : OpenMenu}
-      width={20} height={20}
-      />
-    </div>
-      <header className={cn('z-40 px-6 sm:px-20 py-4 w-full h-full sm:h-auto fixed gap-10 top-0 backdrop-blur-md sm:border-b-2 border-b-miquel-white-200/50', {"opacity-0 -z-10": showMenu})}>
+      <div
+        className="fixed z-50 top-4 right-6 rounded-full p-2 w-9 border hover:bg-miquel-black-300"
+        onClick={() => setshowMenu(!showMenu)}
+      >
+        <IconText
+          src={!showMenu ? "menu-close" : "menu-open"} title={!showMenu ? CloseMenu : OpenMenu}
+          width={20} height={20}
+        />
+      </div>
+      <header className={cn('z-40 px-6 sm:px-20 py-4 w-full h-full sm:h-auto fixed gap-10 top-0 backdrop-blur-md sm:border-b-2 border-b-miquel-white-200/50', { "opacity-0 -z-10": showMenu })}>
         <section className="w-full hidden sm:flex justify-between">
           <Link href="/" className="opacity-70 hover:opacity-100 transform duration-300">
             Miquel Gómez
@@ -38,6 +38,10 @@ export default function Header() {
         </section>
 
         <section className='gap-4 sm:hidden flex flex-col'>
+          <Link href="/" className="opacity-70 hover:opacity-100 transform duration-300 text-2xl font-semibold">
+            <h3>Miquel Gómez</h3>
+          </Link>
+
           <hr className="border-b-miquel-white-200/50" />
 
           <nav className="flex flex-col gap-4">
