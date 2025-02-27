42e2e13204846a8f84820dc8cb95da41570eee40
MaikTheTurboPotato <miquelgc2003@gmail.com>
2025-02-27 18:39:25 +0100

    Fix glowing projects logo

--- app/[locale]/(sections)/Projects.tsx
+++ app/[locale]/(sections)/Projects.tsx
@@ -15,16 +15,16 @@ export default async function Projects({ params }: { params: { locale: string } 
     <section id="Projects" className="w-full flex flex-col gap-6 group/proyects">
       <header className="w-full py-4 border-b-2 border-b-miquel-white-200/50">
         <HeaderButtonLink link="/projects" className="text-5xl font-bold opacity-70 group-hover/proyects:opacity-100 transform duration-300 flex gap-2">
-          <GlowingText>
-            <Icon 
-              src={`html`}
-              width={50}
-              height={50}
-              type={"color"}
-              title={'Projects'}
-            />
-          </GlowingText>
-          {"Proyectos"}
+            <GlowingText className="flex">
+              <Icon 
+                src={`html`}
+                width={50}
+                height={50}
+                type="color"
+                title={'Projects'}
+                />
+            </GlowingText>
+          {t("title")}
         </HeaderButtonLink>
       </header>
 
