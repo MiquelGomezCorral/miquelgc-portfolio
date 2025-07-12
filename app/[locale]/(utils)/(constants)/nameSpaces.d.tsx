33b7a260ed89178691f7f3a81f08e2396cb7bfd2
Miquel Gómez Corral <miquelgc2003@gmail.com>
2025-07-12 13:24:09 +0200

    fixed project names paces

--- app/[locale]/(utils)/(constants)/nameSpaces.d.tsx
+++ app/[locale]/(utils)/(constants)/nameSpaces.d.tsx
@@ -6,5 +6,5 @@ export const allNameSpaces = ['header', 'footer', 'info-section', 'experiences',
 export const projectPagesNameSpaces = ['projects', 'header', 'footer', 'general']
 export const footerNameSpaces =  ["footer"]
 export const infoSectionNameSpaces =  ["info-section"]
-export const projectNameSpaces =  ["info-section"]
+export const projectNameSpaces =  ["projects"]
 export const technologiesNameSpaces =  ["technologies"]
