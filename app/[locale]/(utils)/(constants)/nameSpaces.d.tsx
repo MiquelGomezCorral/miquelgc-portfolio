c65775c96f1ff830c7d162321c6b50f0554c75b4
Miquel Gómez Corral <miquelgc2003@gmail.com>
2025-07-16 19:40:55 +0200

    fixed name space again

--- app/[locale]/(utils)/(constants)/nameSpaces.d.tsx
+++ app/[locale]/(utils)/(constants)/nameSpaces.d.tsx
@@ -3,8 +3,7 @@
 export const allNameSpaces = ['header', 'footer', 'info-section', 'experiences', 'studies', 'projects', 'technologies', 'general'];
 
 
-export const projectPagesNameSpaces = ['projects', 'header', 'footer', 'general']
+export const projectNameSpaces = ['projects', 'header', 'footer', 'general']
 export const footerNameSpaces =  ["footer"]
 export const infoSectionNameSpaces =  ["info-section"]
-export const projectNameSpaces =  ["projects", "footer"]
 export const technologiesNameSpaces =  ["technologies"]
--- app/[locale]/projects/double_pendulum/page.tsx
+++ app/[locale]/projects/double_pendulum/page.tsx
@@ -1,12 +1,12 @@
 
 import initTranslations from "@/app/i18n"
 import TranslationsProvider from "@/app/[locale]/(utils)/TranslationsProvider"
-import {projectPagesNameSpaces} from "@/app/[locale]/(utils)/(constants)/nameSpaces.d"
+import {projectNameSpaces} from "@/app/[locale]/(utils)/(constants)/nameSpaces.d"
 
 import { ProjectPageTemplate } from "@/app/[locale]/projects/project_template"
 import { getProjects } from "@/app/[locale]/(utils)/(constants)/project.text.d"
 
-const i18nNamespaces = projectPagesNameSpaces
+const i18nNamespaces = projectNameSpaces
 export default async function ProjectPage({ params }: { params: any }) {
   const { locale } = await params;
   const { t, resources } = await initTranslations(locale, i18nNamespaces);
