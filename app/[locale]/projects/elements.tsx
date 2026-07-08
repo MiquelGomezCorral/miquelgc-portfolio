d840b53671f68115def265a30b98962a0033f467
Miquel Gómez <miquelgc2003@gmail.com>
2026-07-08 20:40:19 +0200

    fix: render project hover gifs unoptimized

--- app/[locale]/projects/elements.tsx
+++ app/[locale]/projects/elements.tsx
@@ -3,7 +3,6 @@
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { useState } from "react";
 import cn from 'classnames';
 import { IconLink } from "@/app/[locale]/(utils)/(components)/Icons";
 import { ProjectType } from "@/app/[locale]/(utils)/(constants)/project.text.d";
@@ -17,14 +16,12 @@ export function Project({ object, disabled, seeMore, descriptionBelow, linkGit =
   const cardLink = linkGit ? object.github : projectLink
   const linkTarget = linkGit ? "_blank" : undefined
   const linkRel = linkGit ? "noopener noreferrer" : undefined
-  const [gifKey, setGifKey] = useState(0)
 
   const image = object.logo && (
     <Link
       href={cardLink}
       target={linkTarget}
       rel={linkRel}
-      onMouseEnter={() => setGifKey(key => key + 1)}
       className={
         "group/img relative w-full rounded-xl aspect-video col-span-1 flex justify-center lg:justify-end items-center overflow-hidden miquel-frame-dark " +
         "lg:max-h-64 lg:col-span-4 bg-gradient-to-r from-miquel-blue-400 to-indigo-400"
@@ -35,22 +32,16 @@ export function Project({ object, disabled, seeMore, descriptionBelow, linkGit =
         alt={object.title}
         width={800}
         height={450}
-        className={cn("rounded-xl w-[95%] lg:w-10/12 group-hover/img:lg:w-11/12 lg:translate-x-4 transition-[width,transform, opacity] duration-500 aspect-video",{
-          'group-hover/img:opacity': object.gif, 
-          // '': object.gif === undefined
-        }
-        )}
+        className={cn("rounded-xl w-[95%] lg:w-10/12 group-hover/img:lg:w-11/12 lg:translate-x-4 transition-[width,transform,opacity] duration-500 aspect-video")}
       />
       {object.gif && (
         <Image
-        key={gifKey}
         src={object.gif.startsWith("http") ? object.gif : `/assets/projects/${object.gif}.webp`}
         alt={object.title}
         width={800}
         height={450}
-        className={"absolute rounded-xl w-[95%] lg:w-10/12 group-hover/img:lg:w-11/12 lg:translate-x-4 transition-[width,transform,opacity] duration-500 aspect-video group-hover/img:opacity-100 opacity-0"
-
-        }
+        unoptimized
+        className="absolute rounded-xl w-[95%] lg:w-10/12 group-hover/img:lg:w-11/12 lg:translate-x-4 transition-[width,transform,opacity] duration-500 aspect-video opacity-0 group-hover/img:opacity-100"
         />
       )}
     </Link>
@@ -151,4 +142,3 @@ export function SeeMoreProject({ object, text }: { object: ProjectType, text: st
 
   )
 }
-
