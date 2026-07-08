e84a659106b4a426eada32708979d36687c08d84
Miquel Gómez <miquelgc2003@gmail.com>
2026-07-08 19:16:46 +0200

    chore: add footer metadata constants

--- app/[locale]/(utils)/(constants)/constants.text.d.tsx
+++ app/[locale]/(utils)/(constants)/constants.text.d.tsx
@@ -1,5 +1,8 @@
 export const ProjectsFolder = "/projects/"
 
+export const FullName = "Miquel Gómez Corral";
+export const CopyrighYear = "2024";
+
 export const Email = "miquelgc2003@gmail.com";
 
 export const Linkedin = "@Miquel Gómez Corral";
