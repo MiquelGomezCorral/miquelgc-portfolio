f1c8bac440892c180efc9ebabcdce7d836c3f013
Miquel Gómez <miquelgc2003@gmail.com>
2026-07-08 21:06:53 +0200

    release: 1.6.0

--- app/[locale]/(utils)/(constants)/configuration.tsx
+++ app/[locale]/(utils)/(constants)/configuration.tsx
@@ -1,7 +1,7 @@
 class Configuration {
   constructor(
     // =============== General ===============
-    public version = "1.5.1",
+    public version = "1.6.0",
 
     public debounceTime = 300, // ms
     public debounceTimeShort = 100, // ms
